cc4be450d1541b3a5640f03148a606744bbd5317
Parab01a <limosan3667@163.com>
2022-11-08 20:15:08 +0800

    second commit

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 # @Time: 2022/11/8 10:41
 from app import app
+from flask import render_template
 
 
 # 2个路由
@@ -7,4 +8,15 @@ from app import app
 @app.route('/index')
 # 1个视图函数
 def index():
-    return "Hello, World!"
+    user = {'username': 'Barney'}
+    posts = [  # 创建一个列表：帖子。里面元素是两个字典，每个字典里元素还是字典，分别作者、帖子内容。
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
